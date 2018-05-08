63c5afa87512a57b312c30c1db839b9eacd8fe00
HaoyuChen94 <richardchenhaoyu@163.com>
2018-05-07 20:55:07 -0400

    update - Add post part

--- Web_side/Websever.py
+++ Web_side/Websever.py
@@ -2,9 +2,13 @@
 # coding=utf-8
 
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+# common gateway interface
+import cgi
 
 # Handler
 class webserverHandler(BaseHTTPRequestHandler):
+    # get from server
+    # do_get, do_post override the method in BaseHTTPRequestHandler
     def do_GET(self):
         try:
             # see if the url ends with /hello
@@ -12,14 +16,17 @@ class webserverHandler(BaseHTTPRequestHandler):
                 # successful request
                 self.send_response(200)
                 # indicate the sending item is html
-                self.send_header('Content-type', 'text/html')
+                self.send_header('content-type', 'text/html')
                 # end of the HTTP headers
                 self.end_headers()
 
                 # output text
                 output = ""
-                output += "<html><body>Hello!</body></html>"
-                # show message
+                output += "<html><body>"
+                output += "<h1>Hello!</h1>"
+                output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
+                output += "</body></html>"
+                # show output
                 self.wfile.write(output)
                 # print for debugging
                 print output
@@ -28,16 +35,48 @@ class webserverHandler(BaseHTTPRequestHandler):
             if self.path.endswith("/nihao"):
                 self.send_response(200)
                 # charset = utf-8 use Chinese in the html
-                self.send_header('Content-type', 'text/html; charset="utf-8"')
+                self.send_header('content-type', 'text/html; charset="utf-8"')
                 self.end_headers()
 
                 output = ""
-                output += "<html><body>你好! <a href = '/hello'>Back to Hello</a></body></html>"
+                output = ""
+                output += "<html><body>"
+                output += "<h1>你好!</h1> <a href = '/hello'>Back to Hello</a>"
+                output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
+                output += "</body></html>"
                 self.wfile.write(output)
                 print output
                 return
         except:
             self.send_error(404, "File Not Found %s" % self.path)
+
+    def do_POST(self):
+        try:
+            self.send_response(301)
+            # this line is for parse chinese correctly
+            self.send_header('content-type', 'text/html; charset="utf-8"')
+            self.end_headers()
+
+            # get form
+            ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+            # find the datatype of the form
+            if ctype == 'multipart/form-data':
+                fields = cgi.parse_multipart(self.rfile, pdict)
+                # message: form name in the html code
+                messagecontent = fields.get('message')
+
+            output = ""
+            output += "<html><body>"
+            output += " <h2> Okay, how about this: </h2>"
+            # first in the message box
+            output += "<h1> %s </h1>" % messagecontent[0]
+            output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
+            output += "</body></html>"
+            self.wfile.write(output)
+            print output
+        except:
+            pass
+
 # main
 def main():
     try:
