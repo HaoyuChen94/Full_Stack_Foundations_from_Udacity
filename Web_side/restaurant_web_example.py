c26160550da34de42b5f37b116b53ddd81d4c970
HaoyuChen94 <richardchenhaoyu@163.com>
2018-05-07 23:34:39 -0400

    create - add edit delete button, add create page for adding new restaurant

--- Web_side/restaurant_web_example.py
+++ Web_side/restaurant_web_example.py
@@ -20,7 +20,7 @@ class webserverHandler(BaseHTTPRequestHandler):
         try:
             if self.path.endswith("/restaurants"):
                 self.send_response(200)
-                self.send_header('content-type', 'text/html')
+                self.send_header('Content-type', 'text/html')
                 self.end_headers()
 
                 # query the restaurants
@@ -29,11 +29,35 @@ class webserverHandler(BaseHTTPRequestHandler):
                 # output text
                 output = ""
                 output += "<html><body>"
+                output += "<p><a href = '/restaurants/new'>Add a new Restaurant</a></p>"
                 output += "<h1>Hello! Here is the restaurants: </h1>"
                 output += "<p>"
                 for restaurant in restaurants:
                     output += restaurant.name
-                    output += "</br></br></br>"
+                    output += "</br>"
+                    output += "<a href = '#'>Edit</a>"
+                    output += "</br>"
+                    output += "<a href = '#'>Delete</a>"
+                    output += "</br>"
+                output += "</p>"
+                output += "</body></html>"
+
+                # show output
+                self.wfile.write(output)
+                return
+
+            if self.path.endswith("/restaurants/new"):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+
+                # output text
+                output = ""
+                output += "<html><body>"
+                output += "<h2>Add a new restaurant</h2>"
+                output += "<p>"
+                output += '''<form method='POST' enctype='multipart/form-data' action='/restaurants/new'><input name="newRestaurantName" type="text" placeholder = "New Restaurant Name">
+                <input type="submit" value="Create a new restaurant"></form>'''
                 output += "</p>"
                 output += "</body></html>"
 
@@ -43,6 +67,28 @@ class webserverHandler(BaseHTTPRequestHandler):
 
         except IOError:
             self.send_error(404, "File Not Found %s" % self.path)
+
+    def do_POST(self):
+        try:
+            if self.path.endswith("/restaurants/new"):
+                ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+                if ctype == 'multipart/form-data':
+                    fields = cgi.parse_multipart(self.rfile, pdict)
+                    messagecontent = fields.get('newRestaurantName')
+
+                    # create new restaurant
+                    newrestaurant = Restaurant(name=messagecontent[0])
+                    session.add(newrestaurant)
+                    session.commit()
+
+                    # back to the main page
+                    self.send_response(301)
+                    self.send_header('Content-type', 'text/html')
+                    self.send_header('Location', '/restaurants')
+                    self.end_headers()
+
+        except:
+            pass
 
 def main():
     try:
